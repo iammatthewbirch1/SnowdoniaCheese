5234ba904e7dc4119474fe0d2a1f04fa01a71420
Matthew Birch <iammatthewbirch@gmail.com>
2019-07-23 08:51:14 +0100

    Second page html and styling, with some css tidying and added comments to js for features yet to be implemented

--- src/js/section.js
+++ src/js/section.js
@@ -1,6 +1,6 @@
-const timeout = 900;
+const preventScrollMs = 900;
 let scrollable;
-let i = 0;
+let activeSection = document.getElementsByClassName('active')[0];
 
 document.addEventListener( 'wheel', (evt) => {
     if(scrollable){
@@ -12,9 +12,37 @@ document.addEventListener( 'wheel', (evt) => {
 
 setInterval(() => {
     scrollable = true;
-}, timeout);
+}, preventScrollMs);
+
+//add function to prevent longer scroll wheel actions --possibly determined by a high delta?
+//add function to add key presses (arrows)
+//add function to handle touch events (drag library? --touch.js)
+
 
 function moveToNextSection(dir){
-    i++
-    console.log('Move #'+i+' '+dir);
+    let moveSection = false;
+    let activeSectionNo = activeSection.getAttribute('id').slice(-1);
+
+    console.log('Move From #'+activeSectionNo+' '+dir);
+
+    if(dir === 'right' && activeSectionNo < 5) {
+        activeSectionNo++;
+        moveSection = true;
+        activeSection.classList.remove('active');
+        activeSection.classList.add('inactiveLeft');
+    } else if(dir === 'left' && activeSectionNo > 1) {
+        activeSectionNo--;
+        moveSection = true;
+        activeSection.classList.remove('active');
+        activeSection.classList.add('inactiveRight');
+    }
+
+    if(moveSection){
+        activeSection = document.getElementById('section-'+activeSectionNo);
+        activeSection.classList.add('active');
+        activeSection.classList.remove('inactiveLeft');
+        activeSection.classList.remove('inactiveRight');
+    }
+
+    console.log('Move To #'+activeSectionNo+' '+dir);
 }
